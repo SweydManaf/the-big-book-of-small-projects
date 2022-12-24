b07ec64a75e3ae0ed8a8dce00826db09189c8170
Sweyd Manaf <abdulsweyd@gmail.com>
2022-12-24 19:02:53 +0200

    bitmap message
    This program uses a multiline string as a bitmap, a 2D image with only two possible
    colors for each pixel, to determine how it should display a message from the user. In this bitmap, space characters represent an empty space, and all other characters are replaced by characters in the user’s message. The provided bitmap resembles a world map, but you can change this to any image you’d like. The binary simplicity of the space-or-­message-characters system makes it good for beginners. Try experimenting with different messages to see what the results look like!

--- #3 Bitmap Message/bitmap_message.py
+++ #3 Bitmap Message/bitmap_message.py
@@ -4,5 +4,44 @@ import sys
 
 # https://inventwithpython.com/bitmapworld.txt)
 bitmap = """
-
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+....................................................................
 """
+
+print('Bitmap Message')
+print('Enter the message to display with bitmap.')
+message = input('> ')
+
+if message == '':
+    sys.exit()
+
+# Loop over each line in the bitmap
+for line in bitmap.splitlines():
+    # Loop over each character in the line:
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            # Print an empty space since there's a space in the bitmap
+            print(' ', end='')
+        else:
+            # Print a character from the message:
+            print(message[i % len(message)], end='')  # WAU
+    print() # Print a newline
