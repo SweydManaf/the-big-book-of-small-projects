2f29be550855f3a40221762e595a0e1cc599365b
Sweyd Manaf <abdulsweyd@gmail.com>
2023-01-29 22:58:26 +0200

    Create bouncing-dvd-logo.py

---
+++ #Bouncing DVD logo/bouncing-dvd-logo.py
@@ -0,0 +1,7 @@
+"""A bouncing DVD logo animation. You have to be "of a certain age" to appreciate this. Press Ctrl-C to stop"""
+
+# NOTE: Do not reize the terminal window while this program is running.
+
+import sys, random, time
+import bext
+
